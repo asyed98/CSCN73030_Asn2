39d8ebdb47e106e0244d96a2b5e327a2c60f1fa4
Asyed8200 <Asyed8200@conestogac.on.ca>
2022-09-22 16:58:14 -0400

    Step 5: Part 2&3: Version Message and Email Data

--- AliSyed_CSCN73030_A2/Assignment 2/assignment2.cpp
+++ AliSyed_CSCN73030_A2/Assignment 2/assignment2.cpp
@@ -8,11 +8,19 @@ using namespace std;
 typedef struct STUDENT_DATA {
 	string firstName;
 	string lastName;
+	string eMail;
 	string printStudentName() {
 		string fullName = lastName + "," + firstName;
 		return fullName;
 	}
+	string printStudentData() {
+		string fullData = lastName + "," + firstName + "," + eMail;
+		return fullData;
+	}
 }STUDENTDATA;
+
+
+//#define PRE_RELEASE
 
 int main() {
 
@@ -21,27 +29,56 @@ int main() {
 
 	ifstream StudentDataFile("..\\StudentData.txt");
 
-	studentList.clear();
-	while (getline(StudentDataFile, tempDataStorage))
-	{
-		int studentDataFileLength = tempDataStorage.length();
-		STUDENTDATA newStudent;
+	#ifdef PRE_RELEASE
+		cout << "Pre Release Version Running " << endl << endl;
+		studentList.clear();
+		ifstream studentEmailDataFile("..\\StudentData_Emails.txt");
 
-		newStudent.lastName = tempDataStorage.substr(0, tempDataStorage.find(','));
-		newStudent.firstName = tempDataStorage.substr(tempDataStorage.find(',') + 1, studentDataFileLength);
+		while (getline(studentEmailDataFile, tempDataStorage))
+		{
+			int studentDataFileLength = tempDataStorage.length();
 
-		studentList.push_back(newStudent);
-	}
-	StudentDataFile.close();
+			STUDENTDATA newStudent;
+
+			newStudent.lastName = tempDataStorage.substr(0, tempDataStorage.find(','));
+			int lastNameLength = newStudent.lastName.length();
+			int check = tempDataStorage.rfind(',');
+			newStudent.firstName = tempDataStorage.substr(lastNameLength + 1, (tempDataStorage.rfind(',') - lastNameLength - 1));
+			int fullNameLength = lastNameLength + newStudent.firstName.length();
+			newStudent.eMail = tempDataStorage.substr(fullNameLength + 2, (studentDataFileLength - fullNameLength - 2));
+
+			studentList.push_back(newStudent);
+		}
+
+		StudentEmailDataFile.close();
+	#else
+		cout << "Standard Version Running" << endl << endl;
+		studentList.clear();
+		while (getline(StudentDataFile, tempDataStorage))
+		{
+			int studentDataFileLength = tempDataStorage.length();
+			STUDENTDATA newStudent;
+
+			newStudent.lastName = tempDataStorage.substr(0, tempDataStorage.find(','));
+			newStudent.firstName = tempDataStorage.substr(tempDataStorage.find(',') + 1, studentDataFileLength);
+
+			studentList.push_back(newStudent);
+		}
+		StudentDataFile.close();
+	#endif
+
 
 	#ifdef _DEBUG
 		cout << "DEBUG Running" << endl << endl;
+	#ifdef PRE_RELEASE
 
 		for (int i = 0; i < studentList.size(); i++)
+			cout << studentList.at(i).printStudentData() << endl;
+	#else
+		for (int i = 0; i < studentList.size(); i++)
 			cout << studentList.at(i).printStudentName() << endl;
-	
+	#endif	
 	#endif
-
 
 	return 1;
 }
