b5155211a3aa0f4d1e981e40a73854095bb328a2
Asyed8200 <Asyed8200@conestogac.on.ca>
2022-09-22 16:49:16 -0400

    Step 3: Initial Source Code

--- AliSyed_CSCN73030_A2/Assignment 2/assignment2.cpp
+++ AliSyed_CSCN73030_A2/Assignment 2/assignment2.cpp
@@ -5,7 +5,32 @@
 
 using namespace std;
 
+typedef struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+
+}STUDENTDATA;
+
 int main() {
+
+	string tempDataStorage;
+	vector<STUDENTDATA>studentList;
+
+	ifstream StudentDataFile("..\\StudentData.txt");
+
+	studentList.clear();
+	while (getline(StudentDataFile, tempDataStorage))
+	{
+		int studentDataFileLength = tempDataStorage.length();
+		STUDENTDATA newStudent;
+
+		newStudent.lastName = tempDataStorage.substr(0, tempDataStorage.find(','));
+		newStudent.firstName = tempDataStorage.substr(tempDataStorage.find(',') + 1, studentDataFileLength);
+
+		studentList.push_back(newStudent);
+	}
+	StudentDataFile.close();
+
 
 	return 1;
 }
